f33f6e727ab551ef5585b254388351d751c6b560
Benjamim Sonntag <benjamimsonntag@gmail.com>
2019-08-03 17:43:00 +0100

    v0.1.0

--- dist/use-current-position.js
+++ dist/use-current-position.js
@@ -7,18 +7,39 @@ exports.useCurrentPosition = useCurrentPosition;
 
 var _react = require("react");
 
-var _reactUsePromise = _interopRequireDefault(require("react-use-promise"));
+function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _nonIterableRest(); }
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance"); }
 
-function getCurrentPosition(options) {
-  return new Promise(function (resolve, reject) {
-    navigator.geolocation.getCurrentPosition(resolve, reject, options);
-  });
-}
+function _iterableToArrayLimit(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
+
+function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
 
 function useCurrentPosition(options) {
-  return (0, _reactUsePromise.default)((0, _react.useCallback)(function () {
-    return getCurrentPosition(options);
-  }, [options]));
+  var _useState = (0, _react.useState)(),
+      _useState2 = _slicedToArray(_useState, 2),
+      position = _useState2[0],
+      setPosition = _useState2[1];
+
+  var _useState3 = (0, _react.useState)(),
+      _useState4 = _slicedToArray(_useState3, 2),
+      error = _useState4[0],
+      setError = _useState4[1];
+
+  (0, _react.useEffect)(function () {
+    var canceled = false;
+    navigator.geolocation.getCurrentPosition(function (position) {
+      if (!canceled) {
+        setPosition(position);
+      }
+    }, function (error) {
+      if (!canceled) {
+        setError(error);
+      }
+    }, options);
+    return function () {
+      canceled = true;
+    };
+  }, [options]);
+  return [position, error];
 }
